b0f59412fd6b5573444bb253a6f0640e94346c19
Exorcist <dungnguyenx2077@gmail.com>
2023-02-01 16:48:44 +0700

    #21

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,18 @@
+import { json } from 'body-parser';
+import db from '../models/index'
+
 let getHomePage = (req, res) => {
     return res.render('homepage.ejs');
 }
-let getAboutPage = (req, res) => {
-    return res.render('test.ejs');
+let getAboutPage = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+        return res.render('test.ejs', {
+            data: JSON.stringify(data)
+        });
+    } catch (e) {
+        console.log(e);
+    }
 }
 module.exports = {
     getHomePage: getHomePage,
