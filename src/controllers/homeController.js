6673dd0c64d238c8a0cdee0f54f908b447582444
Exorcist <dungnguyenx2077@gmail.com>
2023-02-03 20:32:01 +0700

    #25

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,23 +1,22 @@
 import { json } from 'body-parser';
 //Thực hiện lấy database từ file index
 import db from '../models/index'
+import CRUDService from '../services/CRUDService';
 
 let getHomePage = (req, res) => {
     return res.render('homepage.ejs');
 }
 // async khai báo biến đồng bộ
-// await tạm dừng thực hiện hàm async
-let getAboutPage = async (req, res) => {
-    try {
-        let data = await db.User.findAll(); //Khai báo biến data
-        return res.render('test.ejs', {
-            data: JSON.stringify(data) // Hiển thị data lên website
-        });
-    } catch (e) {
-        console.log(e);
-    }
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('AIZ');
 }
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+    getCRUD: getCRUD,
+    postCRUD: postCRUD
 }
