830b1ea48c31f3fe42a2326832887aa101ddc5d3
Exorcist <dungnguyenx2077@gmail.com>
2023-02-06 21:37:40 +0700

    #28 create button delete

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -17,9 +17,6 @@ let postCRUD = async (req, res) => {
 }
 let displayCRUD = async (req, res) => {
     let data = await CRUDService.getAllUser();
-    console.log('---------------------------');
-    console.log(data);
-    console.log('---------------------------');
     return res.render('displayCRUD.ejs', {
         dataTable: data
     });
@@ -47,12 +44,22 @@ let putCRUD = async (req, res) => {
         dataTable: allUsers
     });
 }
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    if (id) {
+        await CRUDService.deleteUserById(id);
+        return res.send('Delete user succeed!');
+    } else {
+        return res.send('User not found!');
+    }
 
+}
 module.exports = {
     getHomePage: getHomePage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayCRUD: displayCRUD,
     getEditCRUD: getEditCRUD,
-    putCRUD: putCRUD
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD
 }
