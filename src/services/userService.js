b138cd5d17c30f1b14cbcd3782248798528856ae
Exorcist <dungnguyenx2077@gmail.com>
2023-02-13 01:16:31 +0700

    #41 fix  err message

--- src/services/userService.js
+++ src/services/userService.js
@@ -108,21 +108,21 @@ let createNewUser = (data) => {
             if (check == true) {
                 resolve({
                     errCode: 1,
-                    message: 'Your email is already in used! Try another email!'
+                    errMessage: 'Your email is already in used! Try another email!'
+                })
+            } else {
+                let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+                await db.User.create({
+                    email: data.email,
+                    password: hashPasswordFromBcrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    address: data.address,
+                    phonenumber: data.phoneNumber,
+                    gender: data.gender === '1' ? true : false,
+                    roleId: data.roleId
                 })
             }
-            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-            await db.User.create({
-                email: data.email,
-                password: hashPasswordFromBcrypt,
-                firstName: data.firstName,
-                lastName: data.lastName,
-                address: data.address,
-                phonenumber: data.phoneNumber,
-                gender: data.gender === '1' ? true : false,
-                roleId: data.roleId
-            })
-
             resolve({
                 errCode: 0,
                 message: 'OK'
