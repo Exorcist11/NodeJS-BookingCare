717f1f6f06e70f3f98a7ad47cc07367c3785455b
Exorcist <dungnguyenx2077@gmail.com>
2023-02-27 17:15:27 +0700

    #55 display fname, lastname

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -60,7 +60,6 @@ let handleEditUser = async (req, res) => {
 
 let getAllCode = async (req, res) => {
     try {
-
         let data = await userService.getAllCodeService(req.query.type);
         return res.status(200).json(data);
     } catch (error) {
