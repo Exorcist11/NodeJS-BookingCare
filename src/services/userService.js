61957c0404975245048d93f0284779b21824c23b
Exorcist <dungnguyenx2077@gmail.com>
2023-03-04 17:44:11 +0700

    #61 update reference userservice

--- src/services/userService.js
+++ src/services/userService.js
@@ -156,7 +156,7 @@ let deleteUser = (userId) => {
 let updateUserData = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!data.id) {
+            if (!data.id || !data.roleId || !data.positionId || !data.gender) {
                 resolve({
                     errCode: 2,
                     message: 'Missing required parameters'
@@ -170,6 +170,9 @@ let updateUserData = (data) => {
                 user.firstName = data.firstName;
                 user.lastName = data.lastName;
                 user.address = data.address;
+                user.roleId = data.roleId;
+                user.positionId = data.positionId;
+                user.phonenumber = data.phonenumber;
                 await user.save();
 
                 resolve({
